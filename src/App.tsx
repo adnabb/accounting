f2786fbe68166c11766b0695c8941b4a2f37dfbe
vivi <chen.shi.yu@qq.com>
2020-07-31 10:47:03 +0800

    完善路由和页面基础布局

--- src/App.tsx
+++ src/App.tsx
@@ -5,54 +5,82 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  Redirect
 } from "react-router-dom";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  border: 1px solid red;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
+const Main = styled.main`
+  border: 1px solid green;
+  flex-grow: 1;
+  overflow-y: auto;
+`
+const Nav = styled.nav`
+  > ul {
+    display: flex;
+    justify-content: space-around;
+  }
+`
 
 function App() {
   return (
       <Router>
-        <div>
-          <nav>
+        <Wrapper>
+          <Main>
+            <Switch>
+              <Redirect exact from="/" to="/account" />
+              <Route path="/tags">
+                <Tags/>
+              </Route>
+              <Route path="/account">
+                <Account/>
+              </Route>
+              <Route path="/statistics">
+                <Statistics/>
+              </Route>
+              <Route path="*">
+                <NoMatch />
+              </Route>
+            </Switch>
+          </Main>
+          <Nav>
             <ul>
               <li>
-                <Link to="/tags">标签</Link>
+                <Link to="/tags">标签页</Link>
               </li>
               <li>
-                <Link to="/">记一笔</Link>
+                <Link to="/account">记账页</Link>
               </li>
               <li>
-                <Link to="/statistics">统计</Link>
+                <Link to="/statistics">统计页</Link>
               </li>
             </ul>
-          </nav>
-
-          <Switch>
-            <Route path="/tags">
-              <Tags/>
-            </Route>
-            <Route path="/statistics">
-              <Statistics/>
-            </Route>
-            {/* path="/" 必须放在最后面*/}
-            <Route path="/">
-              <Account/>
-            </Route>
-          </Switch>
-        </div>
+          </Nav>
+        </Wrapper>
       </Router>
   );
 }
 
 function Tags() {
-  return <h2>tags</h2>;
+  return <h2>标签页面</h2>;
 }
 
 function Account() {
-  return <h2>account</h2>;
+  return <h2>记账页面</h2>;
 }
 
 function Statistics() {
-  return <h2>statistics</h2>;
+  return <h2>统计页面</h2>;
+}
+
+function NoMatch() {
+  return <h2>页面不存在</h2>
 }
 
 export default App;
