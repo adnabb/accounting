3ae1ea09fa9274ed98a774e283a4c3e9a483ca98
vivi <chen.shi.yu@qq.com>
2020-07-31 22:50:20 +0800

    使用require.context导入icons内的所有icon

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -4,15 +4,25 @@ require('../icons/tag.svg');
 require('../icons/account.svg');
 require('../icons/statistic.svg');
 
+function importAll(require: __WebpackModuleApi.RequireContext) {
+  require.keys().forEach(require);
+}
+
+try {
+  importAll(require.context('../icons', true, /\.svg$/));
+} catch (e) {
+  console.log(e)
+}
+
 type Props = {
   name: string
 }
 
-function Icon (props:Props) {
+function Icon(props: Props) {
   return (
-    <svg className="icon">
-      <use xlinkHref={`#${props.name}`}/>
-    </svg>
+      <svg className="icon">
+        <use xlinkHref={`#${props.name}`}/>
+      </svg>
   )
 }
 
