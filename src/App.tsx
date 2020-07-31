190b9fc5e862a88fc3977e4c50783dfccb572455
vivi <chen.shi.yu@qq.com>
2020-07-31 09:23:19 +0800

    初始化项目和路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/tags">标签</Link>
+              </li>
+              <li>
+                <Link to="/">记一笔</Link>
+              </li>
+              <li>
+                <Link to="/statistics">统计</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route path="/tags">
+              <Tags/>
+            </Route>
+            <Route path="/statistics">
+              <Statistics/>
+            </Route>
+            {/* path="/" 必须放在最后面*/}
+            <Route path="/">
+              <Account/>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
   );
+}
+
+function Tags() {
+  return <h2>tags</h2>;
+}
+
+function Account() {
+  return <h2>account</h2>;
+}
+
+function Statistics() {
+  return <h2>statistics</h2>;
 }
 
 export default App;
